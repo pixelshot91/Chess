cb763bbf211652e4f073942314a9a8684454158b
Julien Gautier <julien.gautier@epita.fr>
2017-05-09 19:08:58 +0200

    [OPTION_PARSER] Add option_parser

---
+++ src/main.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include "boost/program_options.hpp"
+
+#include "parser.hh"
+
+namespace po = boost::program_options;
+
+int main(int argc, char* argv[])
+{
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("help,h", "show usage")
+    ("port,p", po::value<int>(),"select the listening port for the network")
+    ("pgn", po::value<std::string>(), "path to the PGN game file")
+    ("listeners,l", po::value<std::vector<std::string>>()->value_name("path"), "list of paths to listener plugins");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << desc << "\n";
+    return 1;
+  }
+  if (vm.count("port"))
+    std::cout << "port is: " << vm["port"].as<int>() << std::endl;
+  if (vm.count("pgn")) {
+    std::string pgn_path = vm["pgn"].as<std::string>();
+    std::cout << "pgn path is: " << pgn_path << std::endl;
+    std::cout << "Parsing..." << std::endl;
+    Parser parser(pgn_path);
+  }
+  /*if (vm.count("listeners"))
+    std::cout << "listeners are: " << vm["listeners"].as<std::vector<std::string>>() << std::endl;*/
+}
