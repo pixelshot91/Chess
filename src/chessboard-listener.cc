372b36bef51c87922284ced972f646b3a7c0af9a
ROGER Nicolas <nicolas.roger@r06p08.cisco.sm.cri.epita.net>
2017-05-13 17:45:30 +0200

    [Listener] board

--- src/chessboard-listener.cc
+++ src/chessboard-listener.cc
@@ -15,8 +15,52 @@ void ChessboardListener::on_game_finished()
 {}
 void ChessboardListener::on_piece_moved(const PieceType piece, const Position& from, const Position& to)
 {
-  std::cout << "A piece has moooooveed" << std::endl;
+  for (int i = 8; i > 0; --i)
+  {
+    for (unsigned j = 0; j < 8; j++) {
+     auto piece = adapter_[plugin::Position((plugin::File)j,(plugin::Rank) i)];
+      auto background = "";
+      if (piece == nullopt)
+        std::cout << background + " ";
+      else {
+        if(piece.get<plugin::Color> == )
+        {
+          std::cout << background + "Hello, ф or \u0444!";
+        }
+        else
+        {
+          std::cout << background + "Hello, ф or \u0444!";
+        }
+      }
+    }
+    std::cout << std::endl;
+  }
 }
+
+//std::cout << "Hello, ф or \u0444!\n";
+/*
+ \e[07m
+ \e|00m
+ */
+/*
+ for (int i = board_.size() - 1; i >= 0; --i)
+  {
+    for (unsigned j = 0; j < board_[0].size(); j++) {
+      if (piecetype_get(plugin::Position((plugin::File)j,(plugin::Rank) i)) == std::experimental::nullopt)
+        std::cerr << "_ ";
+      else
+        std::cerr << static_cast<char>(piecetype_get(plugin::Position((plugin::File)j,(plugin::Rank) i)).value()) << " ";
+
+      //std::cerr << std::hex << std::setfill('0') << std::setw(2) << (int)board_[i][j] << " ";
+    }
+    std::cerr << std::endl;
+  }
+  */
+
+
+
+
+
 void ChessboardListener::on_piece_taken(const PieceType piece, const Position& at)
 {}
 void ChessboardListener::on_piece_promoted(const PieceType piece, const Position& at)
