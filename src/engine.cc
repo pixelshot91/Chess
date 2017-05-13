1c8bdea0ad3d80bf9c73c0d260c003c3136c0bf0
Helene Ksiezak <ksieza_h@epita.fr>
2017-05-13 20:15:23 +0200

    [NETWORK] engine updated

--- src/engine.cc
+++ src/engine.cc
@@ -28,6 +28,7 @@ int Engine::start()
     return 0;
   }
   else {
+
     network_api::ServerNetworkAPI* clients_[2];
     std::string login[2];
     for (int i = 0; i < 2; ++i) { // Connection
@@ -37,26 +38,37 @@ int Engine::start()
 
     for (int i = 0; i < 2; ++i) { // Initialization
       clients_[i]->send("uci");
-      clients_[i]->receive();
+      if (clients_[i]->receive() != "uciok") return -1;
     }
     for (int i = 0; i < 2; ++i) {
       clients_[i]->send("isready");
       if (clients_[i]->receive() != "readyok") return -1;
     }
-    std::string received_moves;
 
+
+    /* Moves */
+
+    std::string total_moves;
+    std::string client_move;
+
+    /* engine -> client_1 */
     clients_[0]->send("ucinewgame");
     clients_[0]->send("go");
-    std::string new_move = clients_[0]->receive();
-    chessboard_.update(Parser::parse_move(new_move));
-    received_moves += " " + new_move;
 
+    client_move = clients_[0]->receive().substr(9);
+    chessboard_.update(Parser::parse_move(client_move));
+    total_moves += " " + client_move;
+
+    /* engine -> client_2 */
     clients_[1]->send("ucinewgame");
-    clients_[1]->send("position startpos moves " + received_moves);
+    clients_[1]->send("position startpos moves " + total_moves);
     clients_[1]->send("go");
-    clients_[1]->receive();
+    client_move = clients_[1]->receive().substr(9);
 
-
+    /* engine -> client_1 */
+    clients_[0]->send(total_moves);
+    clients_[0]->send("go");
+    client_move = clients_[0]->receive().substr(9);
 
     /*while {
     // EACH player play
