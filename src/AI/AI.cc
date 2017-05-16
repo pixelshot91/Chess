ca82eb65c0fd94e8e6870dcd262beea7866849f5
Helene Ksiezak <ksieza_h@epita.fr>
2017-05-16 19:07:41 +0200

    [AI] Solved git conflict

--- src/AI/AI.cc
+++ src/AI/AI.cc
@@ -1,12 +1,14 @@
 #include "AI.hh"
-  AI::AI(plugin::Color AI_color)
-  AI_color_(AI_color)
-{}
+
+
+Ai::Ai(plugin::Color ai_color) 
+      : board_(), ai_color_(ai_color), opponent_color_(static_cast<plugin::Color>(!static_cast<bool>(ai_color_)))
+{
+}
 
 std::string AI::play_next_move(std::string received_move)
 {
-
-  received_move=received_move;
+  received_move = received_move;
   std::string input;
   input;
   //AI
@@ -38,36 +40,145 @@ int GameControl::Evaluate(ChessBoard _B)
 }
 */
 
-
-std::pair<plugin::Move,int> AI::minimax(int depth , plugin::Color playing_color)
+int Ai::evaluate(const ChessBoard& board)
 {
-    int bestValue = -9999;
-    std::pair<plugin::Move,int> bestMove_value;
-    std::pair<plugin::Move,int> move_value;
+  int queens = 9 * (piece_numbers(board, plugin::PieceType::QUEEN, ai_color_) - piece_numbers(board, plugin::PieceType::QUEEN, opponent_color_));
 
-    for( i = 0; i < 8; i++ )
-      for( j = 0; j < 8; j++ )
+  int rooks = 5 * (piece_numbers(board, plugin::PieceType::ROOK, ai_color_) - piece_numbers(board, plugin::PieceType::ROOK, opponent_color_));
+
+  int bishops = piece_numbers(board, plugin::PieceType::BISHOP, ai_color_) - piece_numbers(board, plugin::PieceType::BISHOP, opponent_color_);
+
+  int knights = piece_numbers(board, plugin::PieceType::KNIGHT, ai_color_) - piece_numbers(board, plugin::PieceType::KNIGHT, opponent_color_);
+
+  int pawns = piece_numbers(board, plugin::PieceType::PAWN, ai_color_) - piece_numbers(board, plugin::PieceType::PAWN, opponent_color_);
+
+
+  int doubled = count_doubled(board, plugin::Color::WHITE) - count_doubled(board, plugin::Color::BLACK);
+
+  int isolated = count_isolated(board, plugin::Color::WHITE) - count_isolated(board, plugin::Color::BLACK);
+
+  return queens + rooks + 3 *(bishops - knights) + pawns + 0.5 * (doubled + isolated); // backward
+}
+
+/*
+std::pair<Move,int> AI::minimax(const ChessBoard& board, int depth ,
+plugin::Color playing_color)
+{
+    if (!depth)
+    {
+      auto eval  = evaluate(board);
+    }
+
+    int bestValue = -9999;
+    std::pair<Move,int> bestMove_value;
+    std::pair<Move,int> move_value;
+
+    history_board_.push_back(&board);
+
+    for (auto i = 0; i < 8; i++)
+      for (auto j = 0; j < 8; j++)
       {
         Position position = Position(static_cast<File> i,
             static_cast<Rank>j)i;
-        if(board_.piecetype_get(position) != std::experimental::nullopt;
-&& board_.color_get(position) == playing_color )
+        if (board_.piecetype_get(position) != std::experimental::nullopt;
+&& board_.color_get(position) == playing_color)
 
           std::vector<Move> moves = bord_.get_possible_actions(position);
-            for(auto move : moves)
+            for (auto move : moves)
             {
               //Moving
+              
               apply_move(move);
               move_value = minimax(depth-1 , !playing_color);
 
               //Save best move
-              if((move_value.first > bestMove_value.second )
+              if ((move_value.first > bestMove_value.second)
                   == (playing_color == AI_color))
                 bestMove_value = value;
               //Undo Move
         }
       }
+    //julien est bete ohhhhhhhh! non mais on l'aime notre juju :D
+    history_board_.pop_back();
     return bestMove_value;
+}
+*/
+
+int Ai::piece_numbers(const ChessBoard& board, plugin::PieceType type,
+plugin::Color color)
+{
+  int count;
+  for (auto i = 0; i < 8; i++)
+  {
+    for (auto j = 0; j < 8; j++)
+    {
+      auto piece_type = board.piecetype_get(plugin::Position(static_cast<plugin::File>(i),
+                                                        static_cast<plugin::Rank>(j)));
+      auto piece_color = board.color_get(plugin::Position(static_cast<plugin::File>(i),
+                                                        static_cast<plugin::Rank>(j)));
+      if (piece_type == type && piece_color == color)
+        count++;
+    }
+  }
+  return count;
 }
 
 
+
+int Ai::count_doubled(const ChessBoard& board, plugin::Color color)
+{
+  auto count = 0;
+  for (auto j = 0; j < 8; j++)
+  {
+    bool present = false;
+    for (auto i = 0; i < 8; i++)
+    { // vertical traversal - not horizontal like we usually do
+      auto piece_type = board.piecetype_get(plugin::Position(static_cast<plugin::File>(i),
+                                            static_cast<plugin::Rank>(j)));
+      auto piece_color = board.color_get(plugin::Position(static_cast<plugin::File>(i),
+                                                        static_cast<plugin::Rank>(j)));
+
+      if (!present && (piece_type == plugin::PieceType::PAWN) && (piece_color == color))
+        present = true;
+      else if (piece_type == plugin::PieceType::PAWN && piece_color == color)
+        count ++;
+    }
+  }
+  return count;
+}
+
+int Ai::count_isolated(const ChessBoard& board, plugin::Color color)
+{
+  int count = 0;
+  bool present = false;
+  int meet = 0;
+
+  for (auto j = 0; j < 8; j++)
+  {
+    for (auto i = 0; i < 8; i++)
+    { // vertical traversal - not horizontal like we usually do
+      auto piece_type = board.piecetype_get(plugin::Position(static_cast<plugin::File>(i),
+                                            static_cast<plugin::Rank>(j)));
+      auto piece_color = board.color_get(plugin::Position(static_cast<plugin::File>(i),
+                                                        static_cast<plugin::Rank>(j)));
+
+      if (piece_type == plugin::PieceType::PAWN && piece_color == color)
+      {
+        if (!present) //first time meeting a pawn
+        {
+          present = true;
+        }
+        meet++;
+      }
+    }
+
+    if (present && !meet)
+    {
+      present = false;
+      count++;
+    }
+    meet = 0;
+  }
+  return count;
+
+}
--- src/AI/AI.hh
+++ src/AI/AI.hh
@@ -1,12 +1,38 @@
-#include <string>
-#include "../Player.hh"
+#pragma once
 
-class AI : public Player
+#include "../history.hh"
+#include "../piece/piece.hh"
+#include "../plugin/color.hh"
+#include "../plugin/listener.hh"
+#include "../plugin/piece-type.hh"
+#include "../plugin/position.hh"
+#include "../chessboard.hh"
+
+#include <experimental/optional>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <utility>
+
+class Ai// : public Player
 {
-  AI(plugin::Color AI_color);
+  public:
+    Ai(plugin::Color ai_color);
+
+  public:
+    int piece_numbers(const ChessBoard& board, plugin::PieceType type, plugin::Color color);
+
+    std::pair<Move, int> minimax(const ChessBoard& board, int depth, plugin::Color playing_color);
+
+    int evaluate(const ChessBoard& board);
+
+    int count_isolated(const ChessBoard& board, plugin::Color color);
+    int count_doubled(const ChessBoard& board, plugin::Color color);
+
   private :
   std::string play_next_move(std::string received_move) override;
     ChessBoard board_;
-    plugin::Color AI_color;
-
-}
+    const plugin::Color ai_color_;
+    const plugin::Color opponent_color_;
+    static std::vector<ChessBoard*> history_board_;
+};
