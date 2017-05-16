06c0fee01574015319306d187a44495965a80720
Julien Gautier <julien.gautier@epita.fr>
2017-05-17 00:00:09 +0200

    [AUXILIARY] Add auxiliary

--- src/plugin-auxiliary.cc
+++ src/plugin-auxiliary.cc
@@ -1,13 +1,22 @@
 #include "plugin-auxiliary.hh"
 
-char PieceTypeToInt(plugin::PieceType type)
-{
-  char i;
-  for (i = 0; plugin::piecetype_array()[i] != type; ++i)
-    continue;
-  if (i >= 6)
-    throw std::invalid_argument("Not a piece Symbol");
-  return i;
+namespace auxiliary {
+  char PieceTypeToInt(plugin::PieceType type)
+  {
+    char i;
+    for (i = 0; plugin::piecetype_array()[i] != type; ++i)
+      continue;
+    if (i >= 6)
+      throw std::invalid_argument("Not a piece Symbol");
+    return i;
+  }
+
+  std::string to_lan(plugin::Position pos)
+  {
+    std::string result;
+    result += static_cast<char>(pos.file_get()) + 'a';
+    result += static_cast<char>(pos.rank_get()) + '1';
+    return result;
+    //return std::string((char)(static_cast<char>(pos.file_get()) + 'a')) + std::string(static_cast<char>(pos.rank_get()) + '1');
+  }
 }
-
-
--- src/plugin-auxiliary.hh
+++ src/plugin-auxiliary.hh
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "plugin/position.hh"
 #include "plugin/piece-type.hh"
 
-char PieceTypeToInt(plugin::PieceType type);
+namespace auxiliary {
+  char PieceTypeToInt(plugin::PieceType type);
+  std::string to_lan(plugin::Position pos);
+}
